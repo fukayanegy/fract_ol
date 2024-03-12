6fa2235302f0bdfad25f693869a09e66d1a4d648
Eito Takahama <etakaham@c1r11s5.42tokyo.jp>
2024-03-12 16:48:03 +0900

    drow mandelbrot color add

--- src/complex_operations/comp_add.c
+++ src/complex_operations/comp_add.c
@@ -6,15 +6,15 @@
 /*   By: etakaham <etakaham@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/14 15:13:10 by etakaham          #+#    #+#             */
-/*   Updated: 2024/02/14 17:20:25 by etakaham         ###   ########.fr       */
+/*   Updated: 2024/03/12 14:56:29 by etakaham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../headers/fract_ol.h"
 
-void	comp_square(t_comp *comp, t_comp *squared_comp)
+void	comp_add(t_comp *a, t_comp *b, t_comp *c)
 {
-	squared_comp->r = comp->r * comp->r - comp->i * comp->i;
-	squared_comp->i = 2 * comp->r * comp->i;
+	c->r = a->r + b->r;
+	c->i = a->i + b->i;
 	return ;
 }
--- src/mandelbrot/drow_mandelbrot.c
+++ src/mandelbrot/drow_mandelbrot.c
@@ -6,58 +6,68 @@
 /*   By: etakaham <etakaham@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/14 15:17:08 by etakaham          #+#    #+#             */
-/*   Updated: 2024/02/23 12:49:40 by etakaham         ###   ########.fr       */
+/*   Updated: 2024/03/12 16:47:03 by etakaham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../headers/fract_ol.h"
 
-static void	mandelbrot(t_comp *c, t_data *img, int i, int j)
-{
-	int		k;
-	t_comp	*z;
-	t_comp	*tmp;
-
-	z = init_comp(0.0, 0.0);
-	tmp = init_comp(0.0, 0.0);
-	k = 0;
-	while (k < MANDELBROT_REPEAT)
-	{
-		comp_square(z, tmp);
-		comp_add(tmp, c, z);
-		if (z->r * z->r + z->i * z->i > E)
-		{
-			my_mlx_pixel_put(img, i, j, 0x00000000);
-			break ;
-		}
-		k++;
-	}
-	free(z);
-	free(tmp);
-}
-
 int	drow_mandelbrot(t_data *img)
 {
-	int		i;
-	int		j;
-	t_comp	*c;
+	t_comp		*start;
+	t_comp		*end;
+	t_comp		*C;
+	t_comp		*dC;
+	t_comp		*z;
+	t_comp		*zN;
+	t_comp		*tmp;
+	t_color		*color;
+	int			i, ix, iy, cl, nc;
 
-	c = init_comp(0.0, 0.0);
-	i = 0;
-	while (i < WIDTH)
+	start = init_comp((-2.3 * img->zoom_rate), (-1.5 * img->zoom_rate));
+	end = init_comp((0.7 * img->zoom_rate), (1.5 * img->zoom_rate));
+	C = init_comp(0.0, 0.0);
+	dC = init_comp((end->r - start->r) / WIDTH, (end->i - start->i) / HEIGHT);
+	z = init_comp(0.0, 0.0);
+	zN = init_comp(0.0, 0.0);
+	tmp = init_comp(0.0, 0.0);
+	nc = MANDELBROT_REPEAT / 256;
+	if (nc <= 0)
+		nc = 1;
+
+	for (C->i = start->i, iy=HEIGHT; C->i <= end->i; C->i += dC->i, iy--)
 	{
-		c->r = ((double)i * 4 / WIDTH - 4 / 2) * img->zoom_rate;
-		j = 0;
-		while (j < HEIGHT)
+		for (C->r = start->r, ix=0; C->r <= end->r; C->r += dC->r, ix++)
 		{
-			c->i = ((double)j * 4 / HEIGHT - 4 / 2) * img->zoom_rate;
-			my_mlx_pixel_put(img, i, j, 0x00FFFFFF);
-			mandelbrot(c, img, i, j);
-			j++;
+			z->i = 0.0;
+			z->r = 0.0;
+			for (i = 0; i < MANDELBROT_REPEAT; i++)
+			{
+				comp_square(z, tmp);
+				comp_add(tmp, C, zN);
+				if (zN->r > E || zN->i > E)
+				{
+					break;
+				}
+				z->r = zN->r;
+				z->i = zN->i;
+			}
+			cl = i / nc;
+			if (cl > 255)
+				cl = 255;
+			color = init_color(cl, cl, cl);
+			my_mlx_pixel_put(img, ix, iy, ft_rgb(color));
+			free(color);
 		}
-		i++;
 	}
+
 	mlx_put_image_to_window(img->mlx_ptr, img->win_ptr, img->img_ptr, 0, 0);
-	free(c);
+	free(start);
+	free(end);
+	free(C);
+	free(dC);
+	free(z);
+	free(zN);
+	free(tmp);
 	return (0);
 }
