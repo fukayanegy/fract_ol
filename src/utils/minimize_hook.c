106c10857a76e652a282fb7ae4c54f0eeb1968e2
Eito Takahama <etakaham@c1r14s9.42tokyo.jp>
2024-03-24 15:13:12 +0900

    remove printf in minimize_hook

--- src/utils/minimize_hook.c
+++ src/utils/minimize_hook.c
@@ -6,7 +6,7 @@
 /*   By: etakaham <etakaham@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/14 18:16:48 by etakaham          #+#    #+#             */
-/*   Updated: 2024/02/23 12:41:48 by etakaham         ###   ########.fr       */
+/*   Updated: 2024/03/24 15:11:20 by etakaham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,6 @@
 
 int	minimize_hook(int keycode, t_data *img)
 {
-	printf("======================\n");
-	printf("minimize!\n");
-	printf("img is %p\n", img);
-	printf("keycode is %d\n", keycode);
-	printf("======================\n");
 	drow_mandelbrot(img);
 	return (1);
 	(void)keycode;
